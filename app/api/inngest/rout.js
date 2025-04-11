ebe4c6014293a58c82b576c03166f36145ab716d
ramwijesooriya <ramwijesooriya@gmail.com>
2025-04-11 13:24:08 +0530

    Final local backup before reset

--- app/api/inngest/rout.js
+++ app/api/inngest/rout.js
@@ -1,10 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+     syncUserCreation,
+     syncUserDeletion,
+     syncUserUpdate
   ],
 });
